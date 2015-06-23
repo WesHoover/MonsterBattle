d6b9bf2e6477bab69786310f8715bacca4b80f94
daniel rendon <daniel.rendonfc@gmail.com>
2015-06-23 16:31:04 -0500

    battle logic random stats

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,17 @@
 class Monster < ActiveRecord::Base
+	def combat(monster1, monster2)
+		if(monster1.attack > monster2.attack)
+			puts("1 wins")
+		elsif(monster2.attack > monster1.attack)
+			puts("2 wins")
+		else
+			puts("Tie")	
+		end
+	end	
+		
 end
+
+	def stats(newMonster)
+		newMonster.attack = random(1..10)
+		newMonster.defense = random(1..10)
+	end	

20a868c50e722351c992d8f8d1d12db0b3648be9
daniel rendon <daniel.rendonfc@gmail.com>
2015-06-23 15:50:57 -0500

    another seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 fartface = Monster.create([{name: "fartface", attack: 2, defense: 2 }, {name: "godzilla", attack: 5, defense: 4}, {name: "swamp thing", attack: 1, defense: 7}])
 monster2 = Monster.create([{name: "mothra", attack: 3, defense: 7}, {name: "max", attack: 1, defense: 0}, {name: "almond", attack: 9, defense: 2}])
 fakematch = Match.create([{monster1: 1}, {monster2: 2}, {winner: 2}])
-fakematch2 = Match.create([{monster1: 11, monster2: 12, winner: 12}])
+fakematch2 = Match.create([{monster1: 1, monster2: 2, winner: 12}])
